555efd6baff9afc34a09ab7d75ee5aafea8c7fcb
Jan <j.polivka5@gmail.com>
2021-06-01 23:39:50 +0300

    newStrat integration test; the refactor happened in the previous commit

--- GrandStrat-Backend/src/test/java/grandstrat/backend/GrandStrat/Backend/data/DataControllerTest.java
+++ GrandStrat-Backend/src/test/java/grandstrat/backend/GrandStrat/Backend/data/DataControllerTest.java
@@ -1,18 +1,33 @@
 package grandstrat.backend.GrandStrat.Backend.data;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+import javax.xml.crypto.Data;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DataControllerTest {
 
     @Autowired
     private StratRepository stratRepo;
+
+    @Autowired
+    private DataController dataController;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
 
     @Test
     void allStrats() throws Exception{
@@ -20,7 +35,22 @@ class DataControllerTest {
                                                                 "Quantity");
         stratRepo.save(strat);
 
-        assertTrue(stratRepo.existsByCountry("TEST_COUNTRY"));
+        assertTrue(this.restTemplate.getForObject("http://localhost:" + port + "/strats",
+                String.class).contains("TEST_COUNTRY"));
+
+        for (StratData element : stratRepo.findByCountry("TEST_COUNTRY"))
+            stratRepo.deleteById(element.getId());
+    }
+
+    @Test
+    void newStrat() throws Exception{
+        
+        JSONObject stratJsonObject = new JSONObject();
+        stratJsonObject.put("country", "TEST_COUNTRY");
+        stratJsonObject.put("idea1", "Religious");
+        stratJsonObject.put("idea2", "Quantity");
+
+        dataController.newStrat(stratJsonObject.toString());
 
         for (StratData element : stratRepo.findByCountry("TEST_COUNTRY"))
             stratRepo.deleteById(element.getId());
